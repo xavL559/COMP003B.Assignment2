636c69494c116fd73f0cb7b8c9c6f18dff006018
Xavier Leftridge <xavierleftridge7@gmail.com>
2023-04-01 22:11:08 -0700

    Finished

---
+++ COMP003B.Assignment2/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
